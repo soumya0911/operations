77e719759ac56ee7b5e6c3c8d670f69a969bf7a4
soumya0911 <suryavanshisoumya5@gmail.com>
2025-05-06 15:32:44 +0530

    save this file

---
+++ BasicOperations.java
@@ -0,0 +1,15 @@
+package ArithmeticOps;
+
+public class BasicOperations {
+    public static void main(String[] args) {
+        int a=15;
+        int b=5;
+        int sum=a+b;
+        int diff=a-b;
+        int prod=a*b;
+        System.out.println("Addition:"+sum);
+        System.out.println("Suntraction:"+diff);
+        System.out.println("Multiplication:"+prod);
+    }
+    
+}
